4b3073933d5c2e1e1a48732a2def4f588910a808
KASAYED <kasayeddev@gmail.com>
2026-01-14 21:06:02 -0800

    implement data provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { GitHubBanner, Refine  } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { Refine } from "@refinedev/core";
+import { DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import routerProvider, {
     DocumentTitleHandler,
     UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import dataProvider from "@refinedev/simple-rest";
-import {BrowserRouter, Outlet, Route, Routes} from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
-import Dashboard from "@/pages/dashboard.tsx";
-import {BookOpen, Home} from "lucide-react";
-import {Layout} from "@/components/refine-ui/layout/layout.tsx";
-import SubjectsList from "@/pages/subjects/list.tsx";
-import SubjectsCreate from "@/pages/subjects/create.tsx";
+import { BookOpen, Home } from "lucide-react";
+import SubjectsList from "./pages/subjects/list";
+import { Layout } from "./components/refine-ui/layout/layout";
+import SubjectsCreate from "./pages/subjects/create";
+import Dashboard from "./Pages/dashboard.tsx";
+
+import { dataProvider } from "./providers/data";
 
 function App() {
     return (
@@ -25,34 +26,42 @@ function App() {
                 <ThemeProvider>
                     <DevtoolsProvider>
                         <Refine
-                            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                            dataProvider={dataProvider}
                             notificationProvider={useNotificationProvider()}
                             routerProvider={routerProvider}
                             options={{
                                 syncWithLocation: true,
                                 warnWhenUnsavedChanges: true,
-                                projectId: "jtNqZy-1VTAjv-OXwtBn",
+                                projectId: "kkWuv7-GgBIfw-P8CGy0",
                             }}
                             resources={[
                                 {
-                                    name: 'dashboard',
-                                    list: '/',
-                                    meta: { label: 'Home', icon: <Home />}
+                                    name: "dashboard",
+                                    list: "/",
+                                    meta: {
+                                        label: "Home",
+                                        icon: <Home />,
+                                    },
                                 },
                                 {
-                                    name: 'subjects',
-                                    list: '/subjects',
-                                    create: '/subjects/create',
-                                    meta: { label: 'Subjects', icon: <BookOpen />}
-                                }
+                                    name: "subjects",
+                                    list: "/subjects",
+                                    create: "/subjects/create",
+                                    meta: {
+                                        label: "Subjects",
+                                        icon: <BookOpen />,
+                                    },
+                                },
                             ]}
                         >
                             <Routes>
-                                <Route element={
-                                    <Layout>
-                                        <Outlet />
-                                    </Layout>
-                                }>
+                                <Route
+                                    element={
+                                        <Layout>
+                                            <Outlet />
+                                        </Layout>
+                                    }
+                                >
                                     <Route path="/" element={<Dashboard />} />
 
                                     <Route path="subjects">
@@ -61,12 +70,12 @@ function App() {
                                     </Route>
                                 </Route>
                             </Routes>
+
                             <Toaster />
                             <RefineKbar />
                             <UnsavedChangesNotifier />
                             <DocumentTitleHandler />
                         </Refine>
-                        <DevtoolsPanel />
                     </DevtoolsProvider>
                 </ThemeProvider>
             </RefineKbarProvider>
